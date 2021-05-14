0feff3cc78696b321ee92b92d3d78da86eb85699
rutwikk <rutwikk@pulsesecure.net>
2021-05-14 18:42:02 +0530

    Add comment in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const { merge } = require('webpack-merge');
 const productionConfigObj = require('./webpack.config.prod.js');
 const developmentConfigObj  = require('./webpack.config.dev.js');
+//why is it empty object. ??
+//because i have covered common configuration in both developmentConfigObj and productionConfigObj.
 const commonConfig = {};
 
 module.exports = env => {
